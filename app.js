950274b22503cb42acaa00fe7e93dc310c9834c7
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-01-25 19:16:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const morgan = require('morgan');
+//const programRoutes = require('./routes/programRoutes');
+const mainRoutes = require('./routes/mainRoutes');
+//const userRoutes = require('./routes/userRoutes');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const dotenv = require('dotenv');
+
+//create app
+const app = express();
+
+//configure app
+let port = process.env.PORT || 8084;
+//Allows access from anywhere
+let host = '0.0.0.0';
+dotenv.config();
+let username = `${process.env.DB_USERNAME}`;
+let password = `${process.env.DB_PASSWORD}`;
+let endURL = `${process.env.END_URL}`;
+let url = 'mongodb+srv://' + username + ':' + password + endURL;
+mongoose.set('strictQuery', true);
+app.set('view engine', 'ejs');
+
+//connect to MongoDB
+mongoose.connect(url)
+.then(() => {
+    //start the server
+    app.listen(port, host, ()=>{
+        console.log('Server is running on port', port);
+    });
+})
+.catch(err=>console.log(err.message));
+
+//mount middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+app.use(methodOverride("_method"));
+
+//Session
+/*app.use(session({
+    secret: `${process.env.SESSION_KEY}`,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 60 * 60 * 1000}, //1 hour
+    store: new MongoStore({mongoUrl: url})
+}));*/
+
+//Flash
+//app.use(flash());
+
+/*app.use((req, res, next) => {
+    res.locals.successMessages = req.flash('success');
+    res.locals.errorMessages = req.flash('error');
+    next();
+});*/
+
+//set up routes
+app.use('/', mainRoutes);
+//app.use('/trips', programRoutes);
+//app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+    let err = new Error('The server cannot locate resource ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    if (!err.status) {
+        err.status = 500;
+        err.message = ('Internal Server Error');
+    } 
+    res.status(err.status);
+    console.log(err.stack);
+    res.render('./error/serverError', {error: err});
+});
+
+
