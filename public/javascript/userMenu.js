43ffc4e666c762f283f7d52752e22723e82ccf0f
Jordan Nguyen <tjordannguyen@gmail.com>
2023-02-20 20:37:49 -0500

    user menu js

---
+++ public/javascript/userMenu.js
@@ -0,0 +1,9 @@
+try {
+    let icon = document.getElementById("userIcon");
+    icon.addEventListener("click", function() {
+        document.querySelector(".drop-down-menu-container").classList.toggle("active");
+        
+    });
+} catch (e) {
+
+} 
