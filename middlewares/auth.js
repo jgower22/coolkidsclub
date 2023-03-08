719a647882c492c4b7d04d54a7c5391ceccf392d
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-08 17:10:49 -0500

    added isAdmin

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 //check if user is a guest
 exports.isGuest = (req, res, next) => {
     if (!req.session.user) {
@@ -16,4 +18,24 @@ exports.isLoggedIn = (req, res, next) => {
         req.flash('error', 'You need to log in first');
         return res.redirect('/users/login');
     }
+};
+
+exports.isAdmin = (req, res, next) => {
+    User.findById(req.session.user)
+        .then(user => {
+            if (user) {
+                if (user.role === 'Admin') {
+                    return next();
+                } else {
+                    let err = new Error('Unauthorized to access the resource');
+                    err.status = 401;
+                    return next(err);
+                }
+            } else {
+                let err = new Error('Cannot find user account. Please log out and log in again.');
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));
 };
