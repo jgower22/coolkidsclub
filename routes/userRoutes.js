d13f0edc86526ea6a9794d57b111f67eb0c1f0d6
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-08 17:11:16 -0500

    added isLoggedIn, isGuest

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const controller = require('../controllers/userController');
+const { isLoggedIn, isGuest } = require('../middlewares/auth.js');
 
 const router = express.Router();
 
-router.get('/new', controller.new);
+router.get('/new', isGuest, controller.new);
 
-router.post('/', controller.addUser);
+router.post('/', isGuest, controller.addUser);
 
-router.get('/login', controller.login);
+router.get('/login', isGuest, controller.login);
 
-router.get('/logout', controller.logout);
+router.post('/login', isGuest, controller.processLogin);
 
-router.post('/login', controller.processLogin);
+router.get('/profile', isLoggedIn, controller.profile);
 
-router.get('/profile', controller.profile);
+router.get('/logout', isLoggedIn, controller.logout);
 
 module.exports = router;
