aedd10d0e2d616dd61d957f1955afa1878e1d725
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-08 17:10:59 -0500

    added isLoggedIn

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const controller = require('../controllers/mainController');
-const { isLoggedIn } = require('../middlewares/auth.js');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, controller.index);
+router.get('/', controller.index);
 
 module.exports = router;
