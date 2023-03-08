f7c1cef7d2d9e704fbc6e01044bca12d4eac898c
Jordan Nguyen <tjordannguyen@gmail.com>
2023-03-08 15:41:37 -0500

    app

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.user = req.session.user||null;
+    res.locals.fullName = req.session.fullName||null;
     res.locals.successMessages = req.flash('success');
     res.locals.errorMessages = req.flash('error');
     next();
