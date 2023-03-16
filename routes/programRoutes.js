704924aa2e1d75b1b9db7809c91c373d3cd4ee07
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-16 00:24:46 -0400

    routes

--- routes/programRoutes.js
+++ routes/programRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const controller = require('../controllers/programController');
 const { isLoggedIn, isAdmin } = require('../middlewares/auth.js');
+const { validateId } = require('../middlewares/validator.js');
 
 const router = express.Router();
 
+//Send all programs
 router.get('/', isLoggedIn, controller.index);
 
+//Send new program form
 router.get('/new', isLoggedIn, isAdmin, controller.newProgram);
+
+//Save new program to database
+router.post('/', isLoggedIn, isAdmin, controller.createProgram);
+
+router.get('/:id', validateId, isLoggedIn, controller.showProgram);
 
 module.exports = router;
