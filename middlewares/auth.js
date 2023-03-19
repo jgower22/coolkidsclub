266ed77a5f14f681243f9581dc5c2ec6b544b524
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-18 23:50:52 -0400

    isPatient

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -39,3 +39,23 @@ exports.isAdmin = (req, res, next) => {
         })
         .catch(err => next(err));
 };
+
+exports.isPatient = (req, res, next) => {
+    User.findById(req.session.user)
+        .then(user => {
+            if (user) {
+                if (user.role === 'Patient') {
+                    return next();
+                } else {
+                    let err = new Error('Unauthorized to access the resource');
+                    err.status = 401;
+                    return next(err);
+                }
+            } else {
+                let err = new Error('Cannot find user account. Please log out and log in again.');
+                err.status = 404;
+                return next(err);
+            }
+        })
+        .catch(err => next(err));
+};
