364126ea079ea1462dffe6b3a41788adf1ae967e
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-21 00:54:05 -0400

    delete confirmation for programs

--- public/javascript/deleteConfirmation.js
+++ public/javascript/deleteConfirmation.js
@@ -2,7 +2,7 @@
 try {
     document.getElementById('delete').addEventListener('click', function(event) {
         //Ask for confirmation
-        if (confirm('Are you sure you want to permanently delete this program? Press OK to delete the trip or press cancel to keep the program.') === false) {
+        if (confirm('Are you sure you want to permanently delete this program? Press OK to delete the program or press cancel to keep the program.') === false) {
             event.preventDefault();
         }
     });
