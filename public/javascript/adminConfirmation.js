d906504a5eac691c8c7d818bfe12dd71ffbbe506
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-23 23:40:40 -0400

    confirmation for unbanning

--- public/javascript/adminConfirmation.js
+++ public/javascript/adminConfirmation.js
@@ -1,10 +1,12 @@
 let makeAdminButtons = document.querySelectorAll('.make-admin');
 let banUserButtons = document.querySelectorAll('.ban-user');
+let unbanUserButtons = document.querySelectorAll('.unban-user');
 
 for (i of makeAdminButtons) {
     i.addEventListener('click', function(event) {
         //Ask for confirmation
-        if (confirm('Are you sure make this user an admin? Press OK to continue or press cancel.') === false) {
+        console.log(i.value);
+        if (confirm('Are you sure make ' + this.value  + ' an admin? Press OK to continue or press cancel.') === false) {
             event.preventDefault();
         }
     });
@@ -13,7 +15,16 @@ for (i of makeAdminButtons) {
 for (i of banUserButtons) {
     i.addEventListener('click', function(event) {
         //Ask for confirmation
-        if (confirm('Are you sure ban this user? Press OK to continue or press cancel.') === false) {
+        if (confirm('Are you sure ban ' + this.value + '? Press OK to continue or press cancel.') === false) {
+            event.preventDefault();
+        }
+    });
+}
+
+for (i of unbanUserButtons) {
+    i.addEventListener('click', function(event) {
+        //Ask for confirmation
+        if (confirm('Are you sure unban ' + this.value + '? Press OK to continue or press cancel.') === false) {
             event.preventDefault();
         }
     });
