e9e59c17f563466b445d351a357c792452515f9d
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-03-27 20:05:23 -0400

    added timestamps

--- models/rsvp.js
+++ models/rsvp.js
@@ -5,6 +5,8 @@ const rsvpSchema = new Schema({
     response: {type: String, required: [true, 'rsvp response is required'], enum: ['yes', 'no'] },
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     program: {type: Schema.Types.ObjectId, ref: 'Program'}
-});
+},
+{timestamps: true}
+);
 
 module.exports = mongoose.model('RSVP', rsvpSchema);
