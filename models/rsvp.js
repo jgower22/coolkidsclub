2c03392300a108ce4072509bb73063a824b081c0
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-04-24 00:13:25 -0400

    rsvp

--- models/rsvp.js
+++ models/rsvp.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const rsvpSchema = new Schema({
     response: {type: String, required: [true, 'rsvp response is required'], enum: ['yes', 'no'] },
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-    program: {type: Schema.Types.ObjectId, ref: 'Program'}
+    user: {type: Schema.Types.ObjectId, required: true, ref: 'User'},
+    program: {type: Schema.Types.ObjectId, required: true, ref: 'Program'}
 },
 {timestamps: true}
 );
