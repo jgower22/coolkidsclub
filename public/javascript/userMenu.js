ec547a7c7d6a15f452c22cd3de78ec846ab6cbb8
Jordan Nguyen <tjordannguyen@gmail.com>
2023-05-03 18:55:03 -0400

    userMenu

--- public/javascript/userMenu.js
+++ public/javascript/userMenu.js
@@ -5,12 +5,15 @@ try {
         console.log(element);
         element.classList.toggle('active');
     });
+
+    const xBtn = document.querySelector('#profile-delete-btn');
+    const dropDownContainer = document.querySelector('.drop-down-menu-container');
+
+    xBtn.addEventListener('click', e => {
+        dropDownContainer.classList.remove('active');
+    })
 } catch (e) {
 
-} 
+}
 
-const xBtn = document.querySelector('#profile-delete-btn');
-const dropDownContainer = document.querySelector('.drop-down-menu-container');
 
-xBtn.addEventListener('click',e=>{ dropDownContainer.classList.remove('active');
-})
