dcfc406c4682d6bc5b8ab5c9ae5bd3c464202a10
tinap97 <58527843+tinap97@users.noreply.github.com>
2023-05-03 17:37:20 -0400

    Added drop down animation

--- public/javascript/userMenu.js
+++ public/javascript/userMenu.js
@@ -1,9 +1,14 @@
 try {
+    const chevronUp = document.querySelector('.fa-chevron-up');
+    const chevronDown = document.querySelector('.fa-chevron-down');
+
     document.getElementById('userIcon').addEventListener('click', e => {
         e.preventDefault();
         let element = document.querySelector(".drop-down-menu-container");
         console.log(element);
         element.classList.toggle('active');
+        chevronDown.classList.toggle('active');
+        chevronUp.classList.toggle('active');
     });
 } catch (e) {
 
