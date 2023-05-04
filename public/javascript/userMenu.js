80c088c89e1e33c3c91eb8eb0642cee89575c557
tinap97 <58527843+tinap97@users.noreply.github.com>
2023-05-04 14:13:49 -0400

    Update userMenu.js

--- public/javascript/userMenu.js
+++ public/javascript/userMenu.js
@@ -2,23 +2,17 @@ try {
     const chevronUp = document.querySelector('.fa-chevron-up');
     const chevronDown = document.querySelector('.fa-chevron-down');
     const chevronIcon = document.querySelectorAll('.chevron-icon');
-    const element = document.querySelector(".drop-down-menu-container");
+    const xBtn = document.querySelector('#profile-delete-btn');
+    const dropDownContainer = document.querySelector('.drop-down-menu-container');
 
     document.getElementById('userIcon').addEventListener('click', e => {
         e.preventDefault();
-        //let element = document.querySelector(".drop-down-menu-container");
+        let element = document.querySelector(".drop-down-menu-container");
         console.log(element);
         element.classList.toggle('active');
         chevronDown.classList.toggle('active');
         chevronUp.classList.toggle('active');
     });
-
-    chevronDown.addEventListener('click', e => {
-        element.classlist.toggle('active');
-    })
-
-    const xBtn = document.querySelector('#profile-delete-btn');
-    const dropDownContainer = document.querySelector('.drop-down-menu-container');
 
     xBtn.addEventListener('click', e => {
         dropDownContainer.classList.remove('active');
