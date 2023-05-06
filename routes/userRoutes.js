bab4c740b9fa0b3c62df3e718710fd1283f106f4
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-05-06 15:00:15 -0400

    routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -33,7 +33,7 @@ router.get('/settings', isLoggedIn, controller.settings);
 
 router.get('/usersJSON', controller.usersJSON);
 
-router.get('/rsvpsJSON', controller.rsvpsJSON);
+router.get('/rsvpsJSON/:id', controller.rsvpsJSON);
 
 router.put('/settings/update-username', requestLimiter(5, 'Too many update username requests. Try again later.'), isLoggedIn, validateUsername, validateResult, controller.updateUsername);
 
