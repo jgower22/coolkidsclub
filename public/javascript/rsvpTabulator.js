25d5451a6cec98f215b95681d00a28e88a6af9ae
jackson.gower@gmail.com <jackson.gower@gmail.com>
2023-05-09 23:55:48 -0400

    rsvpTabulator

--- public/javascript/rsvpTabulator.js
+++ public/javascript/rsvpTabulator.js
@@ -3,8 +3,6 @@ async function makeTable() {
     var paginationSize = 10;
     let url = window.location.href;
     let id = url.substring(url.lastIndexOf('/') + 1, url.length);
-    console.log('url: ' + url);
-    console.log('id: ' + id);
     fetch("/users/rsvpsProgramJSON/" + id).then((response) => {
         if (response.ok) {
             return response.json();
@@ -12,7 +10,6 @@ async function makeTable() {
         throw new Error('Something went wrong');
     })
         .then((programs) => {
-            console.log("users: " + JSON.stringify(programs));
             const rsvpsProgramTable = new Tabulator("#rsvpsProgramTable", {
                 data: programs, //assign data to table
                 layout: "fitColumns", //fit columns to width of table (optional)
@@ -37,7 +34,6 @@ async function makeTable() {
             });
         })
         .catch((error) => {
-            console.log(error);
             const hidden = document.querySelectorAll(".hidden");
             for (let i = 0; i < hidden.length; i++) {
                 hidden[i].style.display = "none";
